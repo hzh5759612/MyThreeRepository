f4d8912c709621c69f7bc96ef40bb1e55f6986e7
823788814 <823788814@qq.com>
2020-08-15 10:36:08 +0800

    团队工程师李四添加了一个delete()

--- src/com/gec/test/TestDemo.java
+++ src/com/gec/test/TestDemo.java
@@ -4,12 +4,20 @@ public class TestDemo {
 
     //团队工程师  张三  写了一个insert()
     public void insert(){
+
         System.out.println("public void insert()-------");
     }
 
     //团队工程师  张三  写了一个update()
     public void update(){
+
         System.out.println("public void update()-------");
+    }
+
+    //团队工程师  李四  添加了一个delete()
+    public void delete(){
+
+        System.out.println("public void delete()-------");
     }
 
 
